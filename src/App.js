5b04722ee1dbed68d4d970ed85b78c700c5d03dd
miaod <miaod@mail.open.com.cn>
2023-03-24 19:43:35 +0800

    提交看板小项目的第一个dev版本;

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>我的看板</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main className="kanban-board">
+        <section className="kanban-column"></section>
+        <section className="kanban-column"></section>
+        <section className="kanban-column"></section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
